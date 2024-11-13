5a2043583908046ccb811be5f1de256b4defa4e0
yih1612 <yih1612@naver.com>
2024-11-13 09:49:00 +0900

    store & reducer & action

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from "redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const num = document.querySelector("span");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reducer: 함수, state를 수정하는 곳
+const reducer = (state = 0, action) => {
+  if (action.type === "ADD") {
+    return state + 1;
+  } else if (action.type === "MINUS") {
+    return state - 1;
+  }
+  return state;
+};
+
+// store: state(데이터)를 저장하는 곳
+// createStore: store을 만듦
+// createStore(reducer)
+const store = createStore(reducer);
+
+// dispatch에 action값을 넣어서 reducer에서 action값에 맞게 state를 수정
+store.dispatch({ type: "ADD" });
+
+console.log(store.getState());
