cc5e128946e7b1bb1cf220d2620711b638fb3fc5
yih1612 <yih1612@naver.com>
2024-11-16 12:33:04 +0900

    #2.3 Delete To Do part Two

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const reducer = (state = [], action) => {
     case ADD_TODO:
       return [{ text: action.text, id: action.id }, ...state];
     case DELETE_TODO:
-      return [];
+      return state.filter((toDo) => toDo.id !== action.id);
     default:
       return state;
   }
@@ -33,7 +33,7 @@ const dispatchAddTodo = (text) => {
 };
 
 const dispatchDeleteToDo = (e) => {
-  const id = e.target.parentNode.id;
+  const id = parseInt(e.target.parentNode.id);
   store.dispatch(deleteTodo(id));
 };
 
