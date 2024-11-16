876a061cbf81c4b00aef2041c488fe60ec53e979
yih1612 <yih1612@naver.com>
2024-11-16 10:13:24 +0900

    1.3 Subscriptions

--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const num = document.querySelector("span");
 
-// reducer: 함수, state를 수정하는 곳
+num.innerText = 0;
+
+// reducer: 함수. state를 수정하는 곳
 const reducer = (state = 0, action) => {
   if (action.type === "ADD") {
     return state + 1;
@@ -19,7 +21,22 @@ const reducer = (state = 0, action) => {
 // createStore(reducer)
 const store = createStore(reducer);
 
-// dispatch에 action값을 넣어서 reducer에서 action값에 맞게 state를 수정
-store.dispatch({ type: "ADD" });
+const onChange = () => {
+  num.innerText = store.getState();
+};
 
-console.log(store.getState());
+// store.subscribe(function);
+// store의 변화가 감지되면 function 실행
+store.subscribe(onChange);
+
+const handleAdd = () => {
+  // dispatch에 action값을 넣어서 reducer에서 action값에 맞게 state를 수정
+  store.dispatch({ type: "ADD" });
+};
+
+const handleMinus = () => {
+  store.dispatch({ type: "MINUS" });
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
