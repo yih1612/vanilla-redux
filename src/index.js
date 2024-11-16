c5f2f66bc3fb6cfa14c6a144fb1255cac9b8ffd7
yih1612 <yih1612@naver.com>
2024-11-16 11:31:20 +0900

    #2.1 State Mutation

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const reducer = (state = [], action) => {
   console.log(action);
   switch (action.type) {
     case ADD_TODO:
-      return [];
+      return [...state, { text: action.text, id: action.id }];
     case DELETE_TODO:
       return [];
     default:
@@ -21,11 +21,13 @@ const reducer = (state = [], action) => {
 
 const store = createStore(reducer);
 
+store.subscribe(() => console.log(store.getState()));
+
 const onSubmit = (e) => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  store.dispatch({ type: ADD_TODO, text: toDo });
+  store.dispatch({ type: ADD_TODO, text: toDo, id: Date.now() });
 };
 
 form.addEventListener("submit", onSubmit);
