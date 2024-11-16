f949434154300939fbe30a875de2e0f2b4c8d2f1
yih1612 <yih1612@naver.com>
2024-11-16 23:54:10 +0900

    #3.1 Connecting the Store

--- src/index.js
+++ src/index.js
@@ -1,64 +1,6 @@
-import { createStore } from "redux";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
 
-const form = document.querySelector("form");
-const input = document.querySelector("input");
-const ul = document.querySelector("ul");
-
-const ADD_TODO = "ADD_TODO";
-const DELETE_TODO = "DELETE_TODO";
-
-const addTodo = (text) => {
-  return { type: ADD_TODO, text, id: Date.now() };
-};
-
-const deleteTodo = (id) => {
-  return { type: DELETE_TODO, id };
-};
-
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD_TODO:
-      return [{ text: action.text, id: action.id }, ...state];
-    case DELETE_TODO:
-      return state.filter((toDo) => toDo.id !== action.id);
-    default:
-      return state;
-  }
-};
-
-const store = createStore(reducer);
-
-const dispatchAddTodo = (text) => {
-  store.dispatch(addTodo(text));
-};
-
-const dispatchDeleteToDo = (e) => {
-  const id = parseInt(e.target.parentNode.id);
-  store.dispatch(deleteTodo(id));
-};
-
-const paintToDos = () => {
-  const toDos = store.getState();
-  ul.innerHTML = "";
-  toDos.forEach((todo) => {
-    const li = document.createElement("li");
-    const btn = document.createElement("button");
-    btn.innerText = "🗑️";
-    btn.addEventListener("click", dispatchDeleteToDo);
-    li.id = todo.id;
-    li.innerText = todo.text;
-    li.appendChild(btn);
-    ul.appendChild(li);
-  });
-};
-
-store.subscribe(paintToDos);
-
-const onSubmit = (e) => {
-  e.preventDefault();
-  const toDo = input.value;
-  input.value = "";
-  dispatchAddTodo(toDo);
-};
-
-form.addEventListener("submit", onSubmit);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
---
+++ src/routes/Home.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export default function Home() {
+  const [text, setText] = useState("");
+
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(text);
+  };
+
+  return (
+    <>
+      <h1>To Do</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={onChange} />
+        <button>Add</button>
+      </form>
+    </>
+  );
+}
